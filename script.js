7b1bafd54920892b34a4893323b1cf0ea8283079
chegx <110959546+chegx@users.noreply.github.com>
2022-12-13 22:25:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-document.querySelector("input").onclick = function() {
-  document.querySelector("body").classList.add("remove");
-};
+const inpObj = document.getElementById("email");
 
 document.querySelector(".btn").onclick = function() {
 
-  var adress = document.querySelector("input").value;
-  document.querySelector(".your-email").innerHTML = adress;
+  if (!inpObj.checkValidity()) {
+    document.body.classList.add("try");
 
-  if (adress.includes("@") && adress.includes(".") && adress.length > 5 && !adress.includes(" ")) {
-    document.querySelector("body").classList.add("thank");
-    document.querySelector(".btn").disabled = true;
-    document.querySelector(".btn").classList.add("thank");
-
-  } else {
-    document.querySelector("body").classList.add("try");
     setTimeout(() => {
-      document.querySelector("body").classList.remove("try")
+      document.body.classList.remove("try");
     }, 2000);
+
+  } else if (inpObj.checkValidity()) {
+    document.body.classList.add("thank");
   }
 
+  document.querySelector('.your-email').innerText = inpObj.value;
+    
 };
 
 document.querySelector(".accept-btn").onclick = function() {
-  document.querySelector("body").classList.remove("thank");
-  document.querySelector(".btn").disabled = false;
-  document.querySelector(".btn").classList.remove("thank");
+  document.body.classList.remove("thank");
 };
